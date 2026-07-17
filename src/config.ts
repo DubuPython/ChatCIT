1c8d5e680524e0ebcc6196b4fc2ef63ee54058c3
Dubu <jethroberboso25@gmail.com>
2026-07-17 15:19:02 +0800

    2 page viewer for handbook and magna carta

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const QUICK_PROMPTS = ["Handbook", "Industry Partners", "Facilities", "Faculty & Teachers"];
 export const ORGANIZATIONS = ["Local Student Council", "SPICE", "CREATIV", "TEECS", "FFTS", "CADS", "AATS", "TO", "CIT BAND", "SMTS","FUFAST"];
 export const MAJORS        = ["Computer Technology", "Food Processing Technology", "Drafting and Digital Arts Technology", "Welding Technology", "Automotive Technology", "Electrical Technology", "Electronics Technology", "Mechanical Technology", "H/VAC Technology"];
-export const DOCUMENTS     = ["Student Handbook", "Curriculum Guide", "Enrollment Form", "Academic Calendar"];
+export const DOCUMENTS     = ["Magna Carta", "Curriculum Guide", "Enrollment Form", "Academic Calendar"];
 export const MID_CHOICES   = ["+ New chat", "Change taskbar mode"];
 // Change this:
 // export const API_URL = "http://localhost:5000/api";
