e12b4a4935b69510a6463b73a570d005a59fbbe7
Dubu <jethroberboso25@gmail.com>
2026-07-19 01:55:29 +0800

    Admin Dashboard Upgrades

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const QUICK_PROMPTS = ["Handbook", "Industry Partners", "Facilities", "Faculty & Teachers"];
+export const QUICK_PROMPTS = ["Handbook", "Industry Partners", "Facilities", "Faculty & Teachers","Magna Carta"];
 export const ORGANIZATIONS = ["Local Student Council", "SPICE", "CREATIV", "TEECS", "FFTS", "CADS", "AATS", "TO", "CIT BAND", "SMTS","FUFAST"];
 export const MAJORS        = ["Computer Technology", "Food Processing Technology", "Drafting and Digital Arts Technology", "Welding Technology", "Automotive Technology", "Electrical Technology", "Electronics Technology", "Mechanical Technology", "H/VAC Technology","Mechatronics Technology","Electronics and Comunnication Technology" ];
-export const DOCUMENTS     = ["Magna Carta", "Curriculum Guide", "Enrollment Form"];
+export const DOCUMENTS     = ["Completion Form","Petition Form","Adding/Changing Form","Dropping Form","Graduation Form","F137/TOR Request Form","Student Clearance Form"];
 export const MID_CHOICES   = ["+ New chat", "Change taskbar mode"];
 // Change this:
 // export const API_URL = "http://localhost:5000/api";
